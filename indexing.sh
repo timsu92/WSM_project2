737e057c86d4fa68984620236adc338b9c614a3c
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-11-26 20:08:17 +0800

    feat(indexing): index both stemmed and non-stemmed

--- indexing.sh
+++ indexing.sh
@@ -4,4 +4,15 @@ python3 -m pyserini.index.lucene \
     --index indexes/collection \
     --generator DefaultLuceneDocumentGenerator \
     --threads $(nproc) \
-    --storePositions --storeDocvectors --storeRaw
+    --storePositions --storeDocvectors --storeRaw \
+    --optimize  # for calculating unique terms
+
+python3 -m pyserini.index.lucene \
+    --collection TrecwebCollection \
+    --input WT2G \
+    --index indexes/nostem_collection \
+    --stemmer none \
+    --generator DefaultLuceneDocumentGenerator \
+    --threads $(nproc) \
+    --storePositions --storeDocvectors --storeRaw \
+    --optimize   # for calculating unique terms
